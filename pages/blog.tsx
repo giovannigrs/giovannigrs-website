0cac4cfb5da9c452a390b34ff2dd04497275d971
giovannigrs <130388231+giovannigrs@users.noreply.github.com>
2023-09-11 15:43:11 -0300

    Ajuste no Blog

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -8,11 +8,22 @@ export default function Blog({ posts }: { posts: any }) {
   return (
     <>
       <div className="container mx-auto py-8 min-h-full">
-        <h3 className="text-xl">All my posts (5)</h3>
-        <div className="my-6 grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-          {posts && posts.map((post: any) => {
-            return <PostBlock key={post.slug} post={post} />;
-          })}
+        <div className="mx-auto max-w-7xl px-6 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+              Artigos
+            </h2>
+            <p className="mt-2 text-lg leading-8 text-gray-600">
+              Veja meus artigos sobre tecnologia e algumas dicas sobre Wordpress e outras ferramentas para criação de sites
+            </p>
+          </div>
+          <h3 className="text-xl">All my posts (5)</h3>
+          <div className="my-6 grid grid-flow-row grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+            {posts &&
+              posts.map((post: any) => {
+                return <PostBlock key={post.slug} post={post} />;
+              })}
+          </div>
         </div>
       </div>
     </>
@@ -21,11 +32,11 @@ export default function Blog({ posts }: { posts: any }) {
 
 export const getStaticProps: GetStaticProps = async () => {
   let posts = await getPosts(100); // retrieve first 100 posts
-  
-  if(!posts){
+
+  if (!posts) {
     posts = null;
   }
-  
+
   return {
     props: {
       posts,
--- pages/index.tsx
+++ pages/index.tsx
@@ -65,7 +65,7 @@ export default function HomePage({ posts }: { posts: any }) {
                     <Link
                       target="_blank"
                       className="rounded-md bg-blue-600 px-3.5 py-2.5 gap-3 text-sm font-semibold text-white shadow-sm hover:bg-blue-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
-                      href="https://acdradvocacia.com.br/"
+                      href={post.href}
                     >
                       Ver projeto
                     </Link>
