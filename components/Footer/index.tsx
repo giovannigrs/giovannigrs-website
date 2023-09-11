37e4d2f5f569575777af33300f20826e08d58b8c
giovannigrs <130388231+giovannigrs@users.noreply.github.com>
2023-09-11 15:30:03 -0300

    Ajustes no footer e index

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -4,6 +4,23 @@ import { GithubIcon, LinkedinIcon } from "@/components/Logos/logos";
 import LogosGithubIcon from "../Logos/GithubIcon";
 
 export const Footer = () => {
+  const links = [
+    {
+      id: 1,
+      titulo: "LinkedIn",
+      href: "https://www.linkedin.com/in/giovanni-garcia-ribeiro-de-souza-0263269a",
+    },
+    {
+      id: 2,
+      titulo: "GitHub",
+      href: "https://github.com/giovannigrs/",
+    },
+    {
+      id: 3,
+      titulo: "Política de Privacidade",
+      href: "/politica-de-privacidade",
+    },
+  ];
   return (
     <footer className="py-4 bg-gray-800 text-white">
       <div className="container mx-auto">
@@ -14,39 +31,46 @@ export const Footer = () => {
               Serviços de Desenvolvimentos de sites Wordpress, otimização SEO de
               sites, customização sob demanda e reestruturação de sites
             </p>
-            <p className="text-gray-300 pt-2">Eng. Giovanni Garcia R. de Souza</p>
-            <small>
-              &copy; GRsouza{" "} {new Date().getFullYear()}
-            </small>
+            <p className="text-gray-300 pt-2">
+              Eng. Giovanni Garcia R. de Souza
+            </p>
+            <small>&copy; GRsouza {new Date().getFullYear()}</small>
           </div>
 
-          <div className="flex flex-col items-center justify-center my-4">
-            <h3 className="text-xl font-bold uppercase text-gray-400">Links</h3>
-            <Link
-              href="https://www.linkedin.com/in/giovanni-garcia-ribeiro-de-souza-0263269a"
-              className="link-hover px-4"
-            >
-              LinkedIn
-            </Link>
-            <Link href="https://github.com/giovannigrs/" className="link-hover px-4">
-              Git Hub
-            </Link>
-            <Link href="/politica-de-privacidade" className="link-hover px-4">
-              política&nbsp;de&nbsp;privacidade
-            </Link>
+          <div className="flex flex-col items-center justify-start my-4">
+            <h3 className="text-xl font-bold uppercase text-gray-400 py-2">
+              Links
+            </h3>
+            {links.map((link) => (
+              <Link
+                key={link.id}
+                href={link.href}
+                className="link-hover py-2 px-4"
+                target="_blank"
+              >
+                {link.titulo}
+              </Link>
+            ))}
           </div>
 
-          <div className="flex flex-col items-center justify-center gap-4 my-4">
-            <h3 className="text-xl font-bold uppercase text-gray-400">Redes Sociais</h3>
+          <div className="flex flex-col items-center justify-start my-4">
+            <h3 className="text-xl font-bold uppercase text-gray-400 py-2">
+              Redes Sociais
+            </h3>
             <div className="flex gap-4">
               <Link
                 href="https://www.linkedin.com/in/giovanni-garcia-ribeiro-de-souza-0263269a"
                 className="bg-gray-300 rounded-full p-3 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-gray-100 duration-300"
+                target="_blank"
               >
-                <LinkedinIcon width={"2em"} height={"2em"}></LinkedinIcon>
+                <LinkedinIcon width={"1.5em"} height={"1.5em"}></LinkedinIcon>
               </Link>
-              <Link href="https://github.com/giovannigrs/" className="bg-gray-300 rounded-full p-3 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-gray-100 duration-300">
-                <GithubIcon width={"2em"} height={"2em"}></GithubIcon>
+              <Link
+                href="https://github.com/giovannigrs/"
+                className="bg-gray-300 rounded-full p-3 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:bg-gray-100 duration-300"
+                target="_blank"
+              >
+                <GithubIcon width={"1.5em"} height={"1.5em"}></GithubIcon>
               </Link>
             </div>
           </div>
